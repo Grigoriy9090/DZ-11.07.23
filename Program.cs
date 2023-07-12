781c2c62c50e47612b1fa237813b96fcf756e1fa
“Grigoriy” <tgo90@mail.ru>
2023-07-12 23:47:38 +0300

    Домашнее задание решено

--- Program.cs
+++ Program.cs
@@ -351,8 +351,8 @@
 //    Console.WriteLine(Math.Pow(i,2));
 // }
 
-// // Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
-// // и находит расстояние между ними в 3D пространстве.
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
 // A(3,6,8); B (2,1,-7); -> 15.84
 // A(7,-5,0); B (1,-1,9); -> 11.53
 
@@ -369,38 +369,6 @@
 // double distance=Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2) + Math.Pow(z2-z1,2));
 
 // Console.WriteLine($"Расстояние между точками: {distance}");
-
-// using System;
-
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Console.WriteLine("Введите координаты первой точки (x1, y1, z1):");
-//         double x1 = Convert.ToDouble(Console.ReadLine());
-//         double y1 = Convert.ToDouble(Console.ReadLine());
-//         double z1 = Convert.ToDouble(Console.ReadLine());
-
-//         Console.WriteLine("Введите координаты второй точки (x2, y2, z2):");
-//         double x2 = Convert.ToDouble(Console.ReadLine());
-//         double y2 = Convert.ToDouble(Console.ReadLine());
-//         double z2 = Convert.ToDouble(Console.ReadLine());
-
-//         double distance = CalculateDistance(x1, y1, z1, x2, y2, z2);
-//         Console.WriteLine($"Расстояние между точками: {distance}");
-//     }
-
-//     static double CalculateDistance(double x1, double y1, double z1, double x2, double y2, double z2)
-//     {
-//         double dx = x2 - x1;
-//         double dy = y2 - y1;
-//         double dz = z2 - z1;
-
-//         double distance = Math.Sqrt(dx * dx + dy * dy + dz * dz);
-//         return distance;
-//     }
-// }
-
 
 // Домашняя задача 19: Напишите программу, которая принимает на вход пятизначное число 
 // и проверяет, является ли число палиндромом.
