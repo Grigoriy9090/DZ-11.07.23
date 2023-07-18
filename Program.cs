ea908cabb2d15737a9f5800efe0e92ff22aaef81
“Grigoriy” <tgo90@mail.ru>
2023-07-18 17:02:08 +0300

    домашнее задание 34, 36, 38

--- Program.cs
+++ Program.cs
@@ -456,14 +456,14 @@
 
 // int GetSumNumber()
 // {
-    // int sum=0;
-    // int count=1;
+// int sum=0;
+// int count=1;
 
-    // while(number>=count)
-    // {
-    //     sum=sum + count;
-    //     count=count+1;
-    // }
+// while(number>=count)
+// {
+//     sum=sum + count;
+//     count=count+1;
+// }
 //     int sum =0;
 
 //     for (int i = 0; i <= number; i++)
@@ -498,7 +498,7 @@
 //         number= number/10;
 //         count=count+1;
 //     }
-    
+
 //     return count;
 // }
 
@@ -522,7 +522,7 @@
 //     {
 //         result= result*i;
 //     }
-    
+
 //     return result;
 // }
 
@@ -600,13 +600,296 @@
 // System.Console.WriteLine();
 
 // второй способ намного лучше!!!!
-Console.Write("Ввидите число a: ");
-int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Ввидите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Ввидите число b: ");
-int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Ввидите число b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
 
-double result=Math.Pow(a,b); 
+// double result=Math.Pow(a,b); 
 
-Console.Write("Результат: " + result);
-System.Console.WriteLine();
+// Console.Write("Результат: " + result);
+// System.Console.WriteLine();
+
+// Задача 31: Задайте массив из 12 элементов, заполненный 
+// случайными числами из промежутка [-9, 9]. Найдите сумму 
+// отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма 
+// положительных чисел равна 29, сумма отрицательных равна -20.
+
+// int arrayLength= 12;
+
+// int[] array = new int[arrayLength];
+
+// int positiveNumber=0;
+// int negNumber=0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i]= new Random().Next(-9, 10);
+//     Console.Write(array[i]+" ");
+
+//     if (array[i]>0)
+//     {
+//         // positiveNumber=positiveNumber+array[i];
+//         positiveNumber+=array[i];
+//     }
+//     else
+//     {
+//         negNumber+=array[i];
+
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine($"Сумма положительных: {positiveNumber} и отрицательных: {negNumber}");
+
+// Задача 32: Напишите программу замена элементов массива:
+//  положительные элементы замените на соответствующие 
+//  отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+// int arrayLength= 4;
+
+// int[] array = new int[arrayLength];
+
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i]= new Random().Next(-9, 10);
+//     Console.Write(array[i]+" ");
+
+// }
+
+// Console.WriteLine();
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i]= array[i]* -1;
+//     Console.Write(array[i]+" ");
+// }
+
+// Задача 33: Задайте массив. Напишите программу, 
+// которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int arrayLength= 4;
+// bool isFind=false;
+
+// int[] array = new int[arrayLength];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i]= new Random().Next(-9, 10);
+//     Console.Write(array[i]+" ");
+
+//     if(num==array[i])
+//     {
+//         isFind=true;
+//     }
+// }
+
+// if (isFind==true)
+// {
+//     System.Console.WriteLine( "Да");
+// }
+// else
+// {
+//     System.Console.WriteLine( "Нет");
+// }
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+//  Найдите количество элементов массива, значения которых 
+//  лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём
+//  решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+// int arrayLength= 123;
+// int countNumber=0;
+
+// int[] array = new int[arrayLength];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i]= new Random().Next(123);
+//     Console.Write(array[i]+" ");
+
+//     if((array[i]>9)&&(array[i]<100))
+//     {
+//         countNumber++;
+//     }
+// }
+
+// System.Console.WriteLine("Кол. = "+ countNumber);
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном
+//  массиве. Парой считаем первый и последний элемент,
+//   второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3   
+// [6 7 3 6] -> 36 21 
+
+// int arrayLength= 5;
+
+// int[] array = new int[arrayLength];
+// int [] array2 = new int[array.Length/2+array.Length % 2];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i]= new Random().Next(10);
+//     Console.Write(array[i]+" ");
+// }
+// Console.WriteLine();
+
+// for (int i = 0; i < array2.Length; i++)
+// {
+//     array2[i]= array[i]*array[arrayLength-1-i];
+
+//     if (i==arrayLength-1-i)
+//     {
+//         array2[i]=array[i];
+//     }
+//     Console.Write(array2[i]+" ");
+// }
+// Console.WriteLine();
+
+
+// Домашнее задание 34: Задайте массив заполненный случайными положительными трехзначными числами.
+// Напишите программу, которая покажет количество четных чисел в массиве.
+// пример решения 
+// [345, 897, 568, 234] -> 2
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         int arrayLangth = 5;
+//         int[] array = new int[arrayLangth];
+//         for (int i = 0; i < arrayLangth; i++)
+//         {
+//             array[i] = new Random().Next(100, 1000);
+//             Console.Write(array[i]+", ");
+//             // (string.Join(", ", arr));
+//         }
+//         int count = 0;
+//         foreach (int num in array)
+//         {
+//             if (count % 2 == 0)
+//             {
+//                 count++;
+//             }
+//         }
+//         Console.Write($"Количество четных чисел в массиве: {count}");
+//         Console.WriteLine();
+//     }
+// }
+
+
+
+// Идеаотное решение
+
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         // создаем массив из 10 случайных трехзначных чисел
+//         int[] arr = new int[5];
+//         Random rand = new Random();
+//         for (int i = 0; i < arr.Length; i++)
+//         {
+//             arr[i] = rand.Next(100, 1000);
+//         }
+//         Console.WriteLine(string.Join(", ", arr));
+
+//         // считаем количество четных чисел в массиве
+//         int count = 0;
+//         foreach (int num in arr)
+//         {
+//             if (num % 2 == 0)
+//             {
+//                 count++;
+//             }
+//         }
+
+//         Console.WriteLine($"Количество четных чисел в массиве: {count}");
+//     }
+// }
+
+
+// Домашняя задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6]-> 0
+
+
+// Идеальное решение:
+// using System;
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         // создаем массив из 4 случайных чисел
+//         int[] arr = new int[4];
+//         Random rand = new Random();
+//         for (int i = 0; i < arr.Length; i++)
+//         {
+//             arr[i] = rand.Next(-10, 10);
+//         }
+//         Console.WriteLine(string.Join(", ", arr));
+
+//         // считаем сумму элементов на нечетных позициях
+//         int sum = 0;
+//         for (int i = 1; i < arr.Length; i += 2)
+//         {
+//             sum += arr[i];
+//         }
+
+//         Console.WriteLine($"Сумма элементов на нечетных позициях: {sum}");
+//     }
+// }
+
+// Домашняя задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         // создаем массив из 5 вещественных чисел
+//         double[] arr = { 3.22, 4.2, 1.15, 77.15, 65.2 };
+//         Console.WriteLine(string.Join(", ", arr));
+
+//         // ищем минимальный и максимальный элементы
+//         double min = arr[0];
+//         double max = arr[0];
+//         for (int i = 1; i < arr.Length; i++)
+//         {
+//             if (arr[i] < min)
+//             {
+//                 min = arr[i];
+//             }
+//             if (arr[i] > max)
+//             {
+//                 max = arr[i];
+//             }
+//         }
+
+//         // выводим разницу между ними
+//         double diff = max - min;
+//         Console.WriteLine($"Разница между максимальным и минимальным элементами: {diff}");
+//     }
+// }
