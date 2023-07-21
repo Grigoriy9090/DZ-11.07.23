cca827af50472eb6178c6626f25c4008e7d8e684
“Grigoriy” <tgo90@mail.ru>
2023-07-21 11:31:38 +0300

    Создал папку

---
+++ HomeWork/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Задача 39: Напишите программу, которая перевернёт
+//  одномерный массив (последний элемент будет на первом
+//   месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+// int [] array = new int[] {1,2,3,4,5};
+
+
+// for (int i = 0; i < array.Length/2; i++)
+// {
+//     int temp = array[i];
+//     array[i]=array[array.Length-1-i];
+//     array[array.Length-1-i]=temp;
+// }
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.Write(array[i]+" ");
+// }
+
+System.Console.WriteLine("Вывод из папки домашнее задание");
